d35489a57ba1c2e7d57857969b578527d2905f68
srishti <srishtivermachamp@gmail.com>
2020-03-21 23:15:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import  Input from './components/Input.jsx';
+import Result from './components/Result.jsx';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
   render() { 
     return ( <React.Fragment>
       <Input />
+      <Result/>
       </React.Fragment> );
   }
 }
